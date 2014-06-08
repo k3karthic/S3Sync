7329da6f46d16e212b8cf31fdd44be8cace5ad2a
Karthic Kumaran Krisnakumar <k3.karthic@live.com>
2014-06-08 21:14:12 +0530

    Add MIT license information

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(
     author='Karthic Kumaran',
     author_email='k3.karthic@live.com',
     packages=['s3sync'],
-    url='http://pypi.python.org/pypi/S3Sync/',
-    license='LICENSE',
+    url='https://github.com/k3karthic/S3Sync',
+    license='MIT',
     description='Sync your files to Amazon S3',
     long_description=open('README.txt').read(),
     install_requires=[
